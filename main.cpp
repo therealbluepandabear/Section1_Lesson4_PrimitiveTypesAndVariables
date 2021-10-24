5b77a4dbed2ee7a8d3cdbe4cc0175f88d98c30c5
xTheCoderx <justaguyoney@gmail.com>
2021-10-24 18:02:38 +1300

    Added information and examples of C++ integral types.

--- main.cpp
+++ main.cpp
@@ -3,5 +3,19 @@
 int main() {
     std::cout << "In this section we will look at the different types provided by the C++ type system.\n";
 
+    std::cout << "C++ provides arithmetic types and void types.\n";
+
+    std::cout << "Arithmetic types are integral and floating-point types.\n";
+    std::cout << "Integral type examples include:\n";
+
+    bool integral_type_one;
+    char integral_type_two;
+    wchar_t integral_type_three;
+    char16_t integral_type_four;
+    char32_t integral_type_five;
+    short integral_type_six;
+    int integral_type_seven;
+    long integral_type_eight;
+
     return 0;
 }
